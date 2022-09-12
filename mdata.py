575d04f82af28ac5cdf8072dd64f91c1bb490557
Hitesh Rahangdale <hitesh.vvr@gmail.com>
2022-09-12 10:01:15 -0400

    some unknown changes in mdata

--- mdata.py
+++ mdata.py
@@ -1,13 +1,14 @@
 from matplotlib.pyplot import axis
 import numpy as np
 import h5py as hd
+import pandas as pd
 
 
 class MData():
     def __init__(self) -> None:
         self.data = None
         self.fname = None
-        self.eventsig = 0xaa55f154
+        # self.eventsig = 0xaa55f154
         self.mdata = None
         self.headerinfo = 1
         self.totalevents = 0
@@ -21,97 +22,27 @@ class MData():
         teventlist = []
         for i in range(len(self.eventsdata)):
             evtstr = {
-                    'event_id':mm[i][0],
-                    'event_type':mm[i][1],
-                    'nu_waveforms':mm[i][2],
-                    'wave_len':mm[i][3],
-                    'base_tmstamp':mm[i][4],
-                    'nu_trigs':mm[i][5],
-                    'triggers':mm[i][6][0]
+                    'event_id':self.eventsdata[i][0],
+                    'event_type':self.eventsdata[i][1],
+                    'nu_waveforms':self.eventsdata[i][2],
+                    'wave_len':self.eventsdata[i][3],
+                    'base_tmstamp':self.eventsdata[i][4],
+                    'nu_trigs':self.eventsdata[i][5],
+                    'triggers':self.eventsdata[i][6][0]
             }
             teventlist.append(evtstr)
         self.eventsdf = pd.DataFrame(teventlist)
+        print(self.eventsdf.shape)
 
-        retrun(self.eventsdf)
+        return(self.eventsdf)
 
     def getdatafromfile(self):
         """
-        Get the data in the form of array of 48x40 (2d array)(48 channel column, and 40 rows which are samples)
+        Load various datas in the current viewer 
         """
-        # GET OFFSET
-        # print("File: ", self.fname)
-        self.offset = self.getoffset()
-        # print("Header Offset: ", self.offset)
-        if self.offset < 0:
-            self.headerinfo = 0
-            offset = 0
-        else:
-            offset = self.offset
-
-        tdata = np.core.records.fromfile(
-            self.fname, formats='(48)int32,(40,48)int32', names='header,data', offset=offset * 4)
-
-        tdata = tdata['data']
-        tdata = tdata.transpose(0, 2, 1)
-        tdata = tdata // (2**8)
-        tdata = 20 * tdata / (2**24)
-
-        # Set Various Data Attributes
-        self.mdata = tdata
-        self.totalevents = len(self.mdata)
-        del tdata
-        return(self.headerinfo)
-
-    def getarea(self, chan):
-        if self.mdata is not None:
-            talldata = self.mdata[:, chan].flatten()
-            return(talldata.sum())
-        else:
-            return(0)
-
-    def getsingle_chan_evnt(self, evtno, chan):
-        if self.mdata is not None:
-            ydata = self.mdata[evtno, chan]
-            xdata = self.timebinwidth * np.arange(len(ydata))
-            return(xdata, ydata)
-
-    def getrangedata(self, llim, hlim, chan):
-        if self.mdata is not None:
-            ydata = self.mdata[llim:hlim, chan].flatten()
-            xdata = np.arange(len(ydata))
-            return(xdata, ydata)
-
-    def gethistdistribution(self, chan):
-        if self.mdata is not None:
-            tdata = self.mdata[:, chan].flatten()
-            counts, edges = np.histogram(tdata, bins=self.bins)
-            return(edges, counts)
-
-    def getstackdata(self, llim, hlim, chan):
-        if self.mdata is not None:
-            singlechandata = self.mdata[llim:hlim, chan]
-            ydata = singlechandata.flatten()
-            xdata = np.tile(
-                np.arange(0, len(singlechandata[0])), len(singlechandata))
-            xdata = xdata*self.timebinwidth
-            return(xdata, ydata)
-
-    def gettimemean(self, llim=0, hlim=-1, chan=0):
-        if self.mdata is not None:
-            if hlim == -1:
-                hlim = len(self.mdata) - 1
-            tmeandata = self.mdata[llim:hlim, chan].mean(axis=0)
-            xdata = np.arange(0, len(tmeandata)) * self.timebinwidth
-            return(xdata, tmeandata)
-
-    def applyvcut(self, vcutval):
-        if self.mdata is not None:
-            pass
-        # print(self.mdata.shape)
-        # # self.mdata = self.mdata[self.mdata<vcutval]
-        # tindx = np.any(np.any(self.mdata<vcutval, axis = 2), axis = 1)
-        # self.mdata = self.mdata[~tindx]
-        # # print("indexshape", tindx.shape)
-        # print(~tindx[:20])
-        # # self.mdata = np.where(self.mdata < vcutval, self.mdata, self.mdata)
-        # print(self.mdata.shape)
+        self.geteventdataframe()
+    
+    def getsinglesdata(self):
+        """
+        Get dataframe for singles data
+        """
